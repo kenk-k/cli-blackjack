5b9fd82678cb3ad33eb10f8043ddc584964c2168
kenk-k <kenk.kaur@gmail.com>
2026-03-13 17:55:47 +0200

    Esialgne test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    static void main() {
+        Kaardipakk kaardipakk = new Kaardipakk();
+        String[] mastid = new String[] {"Poti","Ärtu", "Risti", "Ruutu"};
+        for (String mast: mastid) {
+            for (int j = 2; j < 11; j++) {
+                kaardipakk.lisamine(new Kaart(mast, Integer.toString(j),j));
+            }
+
+        }
+    }
+
 }
