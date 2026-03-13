88ae57fff4908539b7f322b5f966c75d6fe88e12
kenk-k <kenk.kaur@gmail.com>
2026-03-13 17:14:28 +0200

    Loodud esialgne kaardi klass

--- src/Kaart.java
+++ src/Kaart.java
@@ -1,5 +1,34 @@
+import java.util.List;
+
 class Kaart {
-    static void main(String[] args) {
-        System.out.println("tere");
+    private String mast;
+    //2-10, J, Q, K ,A
+    private String number;
+    private int vaartus;
+
+    public Kaart(String mast, String number, int vaartus) {
+        this.mast = mast;
+        this.number = number;
+        this.vaartus = vaartus;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getMast() {
+        return mast;
+    }
+
+    public int getVaartus() {
+        return vaartus;
+    }
+
+    public static int kaeVaartus(List<Kaart> kaardid) {
+        int kaeSumma = 0;
+        for (Kaart kaart: kaardid) {
+            kaeSumma += kaart.vaartus;
+        }
+        return kaeSumma;
     }
 }
